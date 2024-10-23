dcbfba7cd938fb9ae33c23e936022a88fb7c3663
Musawenkosistar <musawenkosi2504@gmail.com>
2024-10-23 22:20:59 +0000

    0x02-redis_basic

--- 0x02-redis_basic/main.py
+++ 0x02-redis_basic/main.py
@@ -1,40 +1,13 @@
 #!/usr/bin/env python3
-"""
-Main file
-"""
-import redis
-import uuid
-from typing import Union, Callable, Optional
+""" Main file """
 
 Cache = __import__('exercise').Cache
 
 cache = Cache()
 
-data = b"hello"
-key = cache.store(data)
-print(key)
+cache.store(b"first")
+print(cache.get(cache.store.__qualname__))
 
-local_redis = redis.Redis()
-print(local_redis.get(key))
-
-class Cache:
-    def __init__(self):
-        self._redis = redis.Redis()
-        self._redis.flushdb()
-
-    def store(self, data: Union[str, bytes, int, float]) -> str:
-        key = str(uuid.uuid4())
-        self._redis.set(key, data)
-        return key
-
-    def get(self, key: str, fn: Optional[Callable] = None) -> Optional[Union[str, bytes, int, float]]:
-        value = self._redis.get(key)
-        if value is not None and fn is not None:
-            return fn(value)
-        return value
-
-    def get_str(self, key: str) -> Optional[str]:
-        return self.get(key, lambda x: x.decode('utf-8'))
-
-    def get_int(self, key: str) -> Optional[int]:
-        return self.get(key, lambda x: int(x))
+cache.store(b"second")
+cache.store(b"third")
+print(cache.get(cache.store.__qualname__))
