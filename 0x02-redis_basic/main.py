2149c16ccf9eef311ef8d8e514b05a036eab9954
Musawenkosistar <musawenkosi2504@gmail.com>
2024-10-23 22:01:53 +0000

    0x02-redis_basic

--- 0x02-redis_basic/main.py
+++ 0x02-redis_basic/main.py
@@ -3,6 +3,8 @@
 Main file
 """
 import redis
+import uuid
+from typing import Union, Callable, Optional
 
 Cache = __import__('exercise').Cache
 
@@ -14,3 +16,25 @@ print(key)
 
 local_redis = redis.Redis()
 print(local_redis.get(key))
+
+class Cache:
+    def __init__(self):
+        self._redis = redis.Redis()
+        self._redis.flushdb()
+
+    def store(self, data: Union[str, bytes, int, float]) -> str:
+        key = str(uuid.uuid4())
+        self._redis.set(key, data)
+        return key
+
+    def get(self, key: str, fn: Optional[Callable] = None) -> Optional[Union[str, bytes, int, float]]:
+        value = self._redis.get(key)
+        if value is not None and fn is not None:
+            return fn(value)
+        return value
+
+    def get_str(self, key: str) -> Optional[str]:
+        return self.get(key, lambda x: x.decode('utf-8'))
+
+    def get_int(self, key: str) -> Optional[int]:
+        return self.get(key, lambda x: int(x))
