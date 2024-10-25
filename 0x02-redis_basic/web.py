b2de20b7b295c5de3bc6e1db3858f352a6848db4
Musawenkosistar <musawenkosi2504@gmail.com>
2024-10-25 03:28:33 +0000

    0x02-redis_basic

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -1,4 +1,7 @@
 #!/usr/bin/env python3
+"""A module with tools for request caching and tracking.
+"""
+
 
 import redis
 import requests
@@ -6,26 +9,34 @@ from functools import wraps
 
 r = redis.Redis()
 
+
 def url_access_count(method):
+    """decorator for get_page function"""
     @wraps(method)
     def wrapper(url):
+        """wrapper function"""
         key = "cached:" + url
         cached_value = r.get(key)
         if cached_value:
             return cached_value.decode("utf-8")
 
+            # Get new content and update cache
         key_count = "count:" + url
         html_content = method(url)
 
         r.incr(key_count)
         r.set(key, html_content, ex=10)
+        r.expire(key, 10)
         return html_content
     return wrapper
 
+
 @url_access_count
 def get_page(url: str) -> str:
+    """obtain the HTML content of a particular"""
     results = requests.get(url)
     return results.text
 
+
 if __name__ == "__main__":
-    print(get_page('http://slowwly.robertomurray.co.uk/delay/1000/url/http://example.com'))
+    get_page('http://slowwly.robertomurray.co.uk')
