0925e89116a19409cc00e986d1243993fceaa861
Musawenkosistar <musawenkosi2504@gmail.com>
2024-10-25 03:21:34 +0000

    0x02-redis_basic

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -1,38 +1,40 @@
-#!/usr/bin/env python3
-"""A module with tools for request caching and tracking"""
+#!/usr/bin/python3
+""" web task """
 
 import requests
 import time
+import redis
 from functools import wraps
 
-cache = {}
+cache = redis.Redis()
+cache.flushdb()
 
-def cache_result(expiration=10):
+def cache_page(expiration=10):
     def decorator(func):
         @wraps(func)
-        def wrapper(url):
-            current_time = time.time()
+        def wrapper(url: str):
             cache_key = f"count:{url}"
-            if url in cache and (current_time - cache[url]['time'] < expiration):
-                cache[cache_key] += 1
-                return cache[url]['content']
-            else:
-                response = func(url)
-                cache[url] = {'content': response, 'time': current_time}
-                cache[cache_key] = 1
-                return response
+            cached_content = cache.get(url)
+            if cached_content:
+                cache.incr(cache_key)
+                return cached_content.decode('utf-8')
+
+            response = func(url)
+            cache.set(url, response, ex=expiration)
+            cache.set(cache_key, 1)
+            return response
         return wrapper
     return decorator
 
-@cache_result(expiration=10)
+@cache_page(expiration=10)
 def get_page(url: str) -> str:
     response = requests.get(url)
     response.raise_for_status()
     return response.text
 
 if __name__ == "__main__":
-    url = "http://slowwly.robertomurray.co.uk/delay/1000/url/http://example.com"
-    print(get_page(url))
-    print(get_page(url))
+    test_url = "http://slowwly.robertomurray.co.uk"
+    print(get_page(test_url))
+    print(get_page(test_url))
     time.sleep(11)
-    print(get_page(url))
+    print(get_page(test_url))
